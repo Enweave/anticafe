cb9e9145586d11a4a8022db546b42a7c23e78356
InWeaver <iv@simplemedia.ru>
2016-06-20 07:30:47 +0500

    admin titles

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,10 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+admin.site.site_header = u'Антикафе'
+admin.site.site_title = u'Антикафе'
+admin.site.index_title = u'Антикафе'
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     # url(r'^', include("animals.urls", namespace="animals")),
